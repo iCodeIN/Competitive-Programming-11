c2c4ce90d44a588335db962211fa199c87461eb3
DhruvPasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-02-04 11:14:07 +0530

    Created Template for Programming Contests.

---
+++ Template/template.cpp
@@ -0,0 +1,40 @@
+//------------------------------------------------------------------------------
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+// #include <cmath>
+// #include <algorithm>
+// #include <unordered_map>
+// #include <map>
+// #include <set>
+// #include <unordered_set>
+//------------------------------------------------------------------------------
+using namespace std;
+//------------------------------------------------------------------------------
+#define FastIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define v(Type) vector<Type>
+#define w(T)  \
+    int T;    \
+    cin >> T; \
+    while (T--)
+#define int long long int
+#define mod 1000000007ll
+#define endl "\n"
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+void solve()
+{
+       
+}
+//------------------------------------------------------------------------------
+int32_t main()
+{
+    FastIO;
+
+    w(T) 
+      solve();
+    
+    return 0;
+}
+//------------------------------------------------------------------------------
