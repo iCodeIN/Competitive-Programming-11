ef054f051038eae782d5d822f79f78d9e367f062
DhruvPasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-05-01 17:35:03 +0530

    .

---
+++ Google/CodeJam Round 1C 2021/Closest_Pick.cpp
@@ -0,0 +1,143 @@
+
+//------------------------------------------------------------------------------
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+// #include <cmath>
+// #include <algorithm>
+// #include <unordered_map>
+// #include <map>
+// #include <set>
+// #include <unordered_set>
+//------------------------------------------------------------------------------
+using namespace std;
+//------------------------------------------------------------------------------
+#define FastIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define v(Type) vector<Type>
+#define w(T)  \
+    int T;    \
+    cin >> T; \
+    while (T--)
+#define int long long int
+#define mod 1000000007ll
+#define endl "\n"
+//------------------------------------------------------------------------------
+// Any fucntion can be called using Math.function_name();
+//------------------------------------------------------------------------------
+class Math
+{
+public:
+    //Returns gcd of two numbers
+    int gcd(int a, int b)
+    {
+        return (a % b == 0) ? b : gcd(b, a % b);
+    }
+
+    //Returns lcm of two numbers
+    int lcm(int a, int b)
+    {
+        return a * (b / gcd(a, b));
+    }
+
+    // Returns flag array isPrime
+    // isPrime[i] = true (if i is Prime)
+    // isPrime[i] = false (if i is not Prime)
+    vector<bool> *seiveOfEratosthenes(const int N)
+    {
+        vector<bool> *isPrime = new vector<bool>(N + 1, true);
+        (*isPrime)[0] = (*isPrime)[1] = false;
+        for (int i = 2; i * i <= N; ++i)
+            if ((*isPrime)[i])
+                for (int j = i * i; j <= N; j += i)
+                    (*isPrime)[j] = false;
+
+        return isPrime;
+    }
+
+    //Returns (x ^ n)
+    int pow(const int &x, int n)
+    {
+        if (n == 0)
+            return 1;
+        int h = pow(x, n / 2);
+        return (n & 1) ? h * h * x : h * h;
+    }
+
+    //Returns (x ^ n) % M
+    int pow(const int &x, int n, const int &M)
+    {
+        if (n == 0)
+            return 1;
+        int h = pow(x, n / 2) % M;
+        return (n & 1) ? (h * h * x) % M : (h * h) % M;
+    }
+
+    //Returns all Primes <= N
+    vector<int> *primesUptoN(const int N)
+    {
+        vector<bool> *isPrime = seiveOfEratosthenes(N);
+        vector<int> *Primes = new vector<int>;
+        if (2 <= N)
+            (*Primes).push_back(2);
+        for (int i = 3; i <= N; i += 2)
+            if ((*isPrime)[i])
+                (*Primes).push_back(i);
+        return Primes;
+    }
+
+} Math;
+//------------------------------------------------------------------------------
+bool isgood(v(int) & A, int x, int y, int choosen)
+{
+    int closeness = INT32_MAX;
+
+    for (int i = 0; i < A.size(); i++)
+        closeness = min(closeness, abs(choosen - A[i]));
+
+    int c = min(abs(x - choosen), abs(y - choosen));
+
+    return c < closeness or (abs(x - choosen) == abs(y - choosen) and c <= closeness);
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    v(int) A(n);
+    for (int &x : A)
+        cin >> x;
+    double max_prob = 0;
+    int ans = 0;
+    for (int i = 1; i <= k; i++)
+        for (int j = i; j <= k; j++)
+        {
+            int count = 0;
+            for (int choosen = 1; choosen <= k; choosen++)
+                if (isgood(A, i, j, choosen))
+                {
+                    // cout << endl;
+                    // cout << i << " " << j << " " << choosen << endl;
+                    count++;
+                }
+            ans = max(count, ans);
+        }
+
+    max_prob = (ans * 1.0) / k;
+
+    cout << max_prob << endl;
+}
+//------------------------------------------------------------------------------
+int32_t main()
+{
+    FastIO;
+    int test = 1;
+    w(T)
+    {
+        cout << "Case #" << test++ << ": ";
+        solve();
+    }
+    return 0;
+}
+//------------------------------------------------------------------------------
