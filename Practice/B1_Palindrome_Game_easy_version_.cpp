d4de183b06ba24ccbe20a347d57794e3b72c327a
DhruvPasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-05-21 06:15:37 +0530

    .

---
+++ Practice/B1_Palindrome_Game_easy_version_.cpp
@@ -0,0 +1,73 @@
+
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+/* author : pasricha_dhruv */
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+// #include <algorithm>
+// #include <bits/stdc++.h>
+// #include <cmath>
+#include <iostream>
+// #include <map>
+// #include <set>
+// #include <queue>
+// #include <stack>
+// #include <unordered_map>
+// #include <unordered_set>
+#include <vector>
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+using namespace std;
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+#define w(T)  \
+    int T;    \
+    cin >> T; \
+    while (T--)
+
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+#define v(Type) vector<Type>
+#define all(x) x.begin(), x.end()
+
+#define int long long int
+#define mod 1000000007ll
+#define endl "\n"
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+    int zero = 0;
+    for (char &x : s)
+        zero += (x == '0');
+    if (zero > 1 and n % 2 == 1 and s[n / 2] == '0')
+        cout << "ALICE";
+    else
+        cout << "BOB";
+    cout << endl;
+}
+
+int32_t main()
+{
+    FastIO;
+
+    w(T)
+        solve();
+
+    return 0;
+}
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
