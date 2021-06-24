32c39bf80af36fbfeef4841542d2fb3f73cd70c5
DhruvPasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-06-24 23:59:51 +0530

    .

---
+++ Number Theory-1/Prime_Seive/Printing_some_primes.cpp
@@ -0,0 +1,81 @@
+
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+/* author : pasricha_dhruv */
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+// #include <algorithm>
+// #include <bits/stdc++.h>
+// #include <cmath>
+#include <iostream>
+// #include <map>
+// #include <set>
+// #include <queue>
+// #include <stack>
+// #include <unordered_map>
+// #include <unordered_set>
+#include <vector>
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+using namespace std;
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+#define w(T)  \
+    int T;    \
+    cin >> T; \
+    while (T--)
+
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+#define v(Type) vector<Type>
+#define all(x) x.begin(), x.end()
+
+// #define int long long int
+#define mod 1000000007ll
+#define endl "\n"
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+int N = 1e8;
+vector<bool> isPrime(N + 1, true);
+
+void solve()
+{
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= N; i++)
+    {
+        if (isPrime[i])
+            for (int j = i * i; j <= N; j += i)
+                isPrime[j] = false;
+    }
+    int count = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (isPrime[i])
+        {
+            ++count;
+            if (count % 100 == 1)
+                cout << i << endl;
+        }
+    }
+
+    return;
+}
+
+int32_t main()
+{
+    FastIO;
+
+    // w(T)
+    solve();
+
+    return 0;
+}
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
