b4a62a19f3ae85aa034e28b40b31b9c3581eb044
DhruvPasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-07-29 22:00:27 +0530

    .

---
+++ CodeForces/735 (Div 2)/D_Diane.cpp
@@ -0,0 +1,89 @@
+
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+/* author : pasricha_dhruv */
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+// #include <algorithm>
+// #include <bits/stdc++.h>
+// #include <cmath>
+#include <iostream>
+// #include <map>
+// #include <set>
+// #include <queue>
+// #include <stack>
+// #include <unordered_map>
+// #include <unordered_set>
+#include <vector>
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+using namespace std;
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+#define w(T)  \
+    int T;    \
+    cin >> T; \
+    while (T--)
+
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+#define v(Type) vector<Type>
+#define all(x) x.begin(), x.end()
+
+#define int long long int
+#define mod 1000000007ll
+#define endl "\n"
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    if (n <= 26)
+    {
+
+        for (int i = 0; i < n; i++)
+            cout << (char)('a' + i);
+    }
+    else
+    {
+        int v = n - 25;
+        char lb = 'b';
+        char ub = 'z';
+        if (v % 2 == 0)
+            ub--, v++;
+
+        int f = v / 2;
+        int e = v - v / 2;
+
+        while (f--)
+            cout << 'a';
+        while (lb <= ub)
+            cout << lb++;
+        while (e--)
+            cout << 'a';
+    }
+
+    cout << endl;
+}
+
+int32_t main()
+{
+    FastIO;
+
+    w(T)
+        solve();
+
+    return 0;
+}
+
+// // //  // // //  // // //  // // // // // //  // // // // // //  // // //
